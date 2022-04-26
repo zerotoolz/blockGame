1da48cded80df0d71c445920f28a2e849b9255c0
Anatoliy Lut <51475542+zerotoolz@users.noreply.github.com>
2022-04-26 18:03:28 +0800

    main

--- zombiecontract.sol
+++ zombiecontract.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.4.19; //solidity version
 
 contract ZombieFactory { //contract name
 
-    event NewZombie(uint zombieId, string name, uint dna);
+    event NewZombie(uint zombieId, string name, uint dna); //event new zombie check
 
     uint dnaDigits = 16; //zombie skin
     uint dnaModulus = 10 ** dnaDigits; //zombie skin check = exact 16 digits
@@ -15,17 +15,17 @@ contract ZombieFactory { //contract name
 
     Zombie[] public zombies; //zombie array structure stogare with public access
 
-    function _createZombie (string _name, uint _dna) private {
-        uint id = zombies.push(Zombie(_name, _dna)) - 1;
-        NewZombie(id, _name, _dna);
+    function _createZombie (string memory _name, uint _dna) private {
+        uint id = zombies.push(Zombie(_name, _dna)) - 1; // recently new zombie addedid
+        NewZombie(id, _name, _dna); //new zombie event related 
     } 
 
-    function _generateRandomDna(string _str) private view returns (uint) {
+    function _generateRandomDna(string memory _str) private view returns (uint) {
         uint rand = uint(keccak256(_str)); //keccakfrom _str
         return rand % dnaModulus; // zombie skin with exact 16 digits
     }
     
-        function createRandomZombie(string _name) public { //random, zombie creation
+        function createRandomZombie(string memory _name) public { //random, zombie creation
         uint randDna = _generateRandomDna(_name); // skin and name creation 
         _createZombie(_name, randDna); // creation
     
