0e61ce42c8c2c71f37716cd479a7d41e7bc3e2a3
Anatoliy Lut <51475542+zerotoolz@users.noreply.github.com>
2022-04-28 15:47:13 +0800

    main

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -2,14 +2,31 @@ pragma solidity ^0.4.19;
 
 import "./zombiefactory.sol";
 
+contract KittyInterface { // interface for zombie feeding from kitty related contract and it's function
+  function getKitty(uint256 _id) external view returns ( //receiving kitty's data
+    bool isGestating,
+    bool isReady,
+    uint256 cooldownIndex,
+    uint256 nextActionAt,
+    uint256 siringWithId,
+    uint256 birthTime,
+    uint256 matronId,
+    uint256 sireId,
+    uint256 generation,
+    uint256 genes
+  );
+}
+
 contract ZombieFeeding is ZombieFactory {
+
+  address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
 
   function feedAndMultiply(uint _zombieId, uint _targetDna) public {
     require(msg.sender == zombieToOwner[_zombieId]);
     Zombie storage myZombie = zombies[_zombieId];
-    _targetDna = _targetDna % dnaModulus; // ensure zombie skin with exact 16 digits
-    uint newDna = (myZombie.dna + _targetDna) / 2; // everage meening between myZombie and _targetDna
-    _createZombie("NoName", newDna); // new zombie creation 
+    _targetDna = _targetDna % dnaModulus;
+    uint newDna = (myZombie.dna + _targetDna) / 2;
+    _createZombie("NoName", newDna);
   }
 
 }
