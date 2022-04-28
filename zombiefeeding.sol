a6ef18dfb95cc911ff6345bb5288ea3f26fc4e3c
Anatoliy Lut <51475542+zerotoolz@users.noreply.github.com>
2022-04-28 15:08:11 +0800

    main

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -1,8 +1,12 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.4.19; //solidity version
+pragma solidity ^0.4.19;
 
 import "./zombiefactory.sol";
 
 contract ZombieFeeding is ZombieFactory {
+
+  function feedAndMultiply(uint _zombieId, uint _targetDna) public { //new function 
+    require(msg.sender == zombieToOwner[_zombieId]); //ensure zombie owner the same person who sent the zombie 
+    Zombie storage myZombie = zombies[_zombieId]; // received zombie from zombies storage
+  } 
 
 }
