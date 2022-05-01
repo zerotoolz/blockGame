0925e477681a70f33f3c8e38dfbcae5917a99de3
Anatoliy Lut <51475542+zerotoolz@users.noreply.github.com>
2022-05-01 18:15:53 +0800

    main

--- zombiefactory.sol
+++ zombiefactory.sol
@@ -14,6 +14,8 @@ contract ZombieFactory is Ownable {//contract name (ownable for ownable.sol), th
         uint dna; // zombie skin
         uint32 level; //zombie level
         uint32 readyTime; //when can eat next
+        uint32 level; //zombie level
+        uint32 readyTime; //cooldown time
     }
 
     Zombie[] public zombies; //zombie array structure stogare with public access
@@ -22,7 +24,7 @@ contract ZombieFactory is Ownable {//contract name (ownable for ownable.sol), th
     mapping (address => uint) ownerZombieCount; //exact zombie number for this owmner address
 
     function _createZombie (string memory _name, uint _dna) internal {
-        uint id = zombies.push(Zombie(_name, _dna)) - 1; // recently new zombie addedid
+        uint id = zombies.push(Zombie(_name, _dna,  1, uint32(now + cooldownTime))) - 1; // recently new zombie added with name, DNA, level, cooldown time for next action
         zombieToOwner[id] = msg.sender; //new zombie id chains with it's owner address for mapping
         ownerZombieCount[msg.sender]++; //increising (js ++) zombie count for it's owner in mapping
         NewZombie(id, _name, _dna); //new zombie event related 
