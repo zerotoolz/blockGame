406bd7e1fe1f5a5376aef843456faadd86d978cc
Anatoliy Lut <51475542+zerotoolz@users.noreply.github.com>
2022-05-03 13:40:24 +0800

    main

--- zombiehelper.sol
+++ zombiehelper.sol
@@ -3,9 +3,20 @@ pragma solidity ^0.4.19;
 import "./zombiefeeding.sol";
 
 contract ZombieHelper is ZombieFeeding {
+
   modifier aboveLevel(uint _level, uint _zombieId) { //modifier checks zombie level
   require(zombies[_zombieId].level >= _level); //level must be more than
   _;
 }
+
+  function changeName(uint _zombieId, string _newName) external aboveLevel(2, _zombieId) { 
+    require(msg.sender == zombieToOwner[_zombieId]);
+    zombies[_zombieId].name = _newName;
+  }
+
+  function changeDna(uint _zombieId, uint _newDna) external aboveLevel(20, _zombieId) {
+    require(msg.sender == zombieToOwner[_zombieId]);
+    zombies[_zombieId].dna = _newDna;
+  }
 
 }
