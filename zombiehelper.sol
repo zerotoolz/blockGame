cff429f3d8e84f24089701765993ece50c94ea2c
Anatoliy Lut <51475542+zerotoolz@users.noreply.github.com>
2022-05-03 13:15:05 +0800

    main

--- zombiehelper.sol
+++ zombiehelper.sol
@@ -3,7 +3,9 @@ pragma solidity ^0.4.19;
 import "./zombiefeeding.sol";
 
 contract ZombieHelper is ZombieFeeding {
-
- 
+  modifier aboveLevel(uint _level, uint _zombieId) {
+  require(zombies[_zombieId].level >= _level);
+  _;
+}
 
 }
