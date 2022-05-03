9d13644d904edf230bf8564dabed68496f0b4831
Anatoliy Lut <51475542+zerotoolz@users.noreply.github.com>
2022-05-03 13:16:16 +0800

    main

--- zombiehelper.sol
+++ zombiehelper.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.4.19;
 import "./zombiefeeding.sol";
 
 contract ZombieHelper is ZombieFeeding {
-  modifier aboveLevel(uint _level, uint _zombieId) {
-  require(zombies[_zombieId].level >= _level);
+  modifier aboveLevel(uint _level, uint _zombieId) { //modifier checks zombie level
+  require(zombies[_zombieId].level >= _level); //level must be more than
   _;
 }
 
