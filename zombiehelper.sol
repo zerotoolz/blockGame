38a8a9fb5374547e3a80fb5f8140865bb01f6ca5
Anatoliy Lut <51475542+zerotoolz@users.noreply.github.com>
2022-05-08 13:58:53 +0800

    main

--- zombiehelper.sol
+++ zombiehelper.sol
@@ -19,8 +19,14 @@ contract ZombieHelper is ZombieFeeding {
     zombies[_zombieId].dna = _newDna;
   }
   function getZombiesByOwner(address _owner) external view returns(uint[]) {
-    uint[] memory result = new uint[](ownerZombieCount[_owner]); 
+    uint[] memory result = new uint[](ownerZombieCount[_owner]);
+    uint counter = 0; 
+    for (uint i = 0; i < zombies.length; i++) {
+      if (zombieToOwner[i] == _owner) {
+        result[counter] = i;
+        counter++;
+      }
+    }
     return result;
-
   }
 }
