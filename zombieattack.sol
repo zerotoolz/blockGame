70a045ec20a28ad789b43b377eaf42c80e6290f3
Anatoliy Lut <51475542+zerotoolz@users.noreply.github.com>
2022-05-11 15:42:17 +0800

    main

--- zombieattack.sol
+++ zombieattack.sol
@@ -2,10 +2,15 @@ pragma solidity ^0.4.19;
 import "./zombiehelper.sol";
 contract ZombieBattle is ZombieHelper {
   uint randNonce = 0; 
+  uint attackVictoryProbability = 70;
 
   function randMod(uint _modulus) internal returns(uint) {
     randNonce++;
     return uint(keccak256(now, msg.sender, randNonce)) % _modulus;
 
+  }
+
+  function attack(uint _zombieId, uint _targetId) external {
+      
   }
 }
